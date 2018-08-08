1472bc06a24fd1bf9b6a4311ffe1453e41c528be
Himanshu Arora <himanshuarora323@gmail.com>
2018-08-08 10:50:16 +0530

    jj

--- Angular/routing-start/src/app/app.module.ts
+++ Angular/routing-start/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {Routes, RouterModule} from "@angular/router"
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -26,6 +25,8 @@ const appRoutes:Routes=[{path:"",component:HomeComponent},
 ];
 @NgModule({
   declarations: [
+    
+    
     AppComponent,
     HomeComponent,
     UsersComponent,
